b0ed85651b2383f204815c013cdbb9a0a19af2f1
luizlemes <luiz.lemes@alunos.ifsuldeminas.edu.br>
2023-04-04 08:47:29 -0300

    aula13

---
+++ aula12/rodape.php
@@ -0,0 +1,12 @@
+
+    <hr>
+
+<!-- Rodape-->
+<footer class="bg-dark h-4 text-white text-center">
+          Meu site de noticias @ 2023
+</footer>
+ <!-- Final de rodapé-->         
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
